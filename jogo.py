333ea5cd8fdb2f45bad0e272fce892764e974c37
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-08-04 14:12:21 +0000

    aula 04

--- jogo.py
+++ jogo.py
@@ -2,11 +2,58 @@
 print("##           ")
 print("##   ########")  
 print("##   ##    ##")
+print("Jogo do Julio")         
 print("##   ##    ##")
-print("##   ##    ##")
-print("##   ##    ##")
-print("##         ##")
 print("##         ##")
 print("#######    ##")
 print("           ##")
-print("JOGO DE ADIVINHAÇÃO")
+
+numeroSecreto = random.randragem(0,100)
+totalTentativas = 0
+pontos = 100
+ 
+print("Qual níveis de dificuldade?  ")
+print("(1) - fácil (2) - médio (3) - difícil ")
+
+nivel = int(input("Escolha um nível"))
+
+if(nivel == 1):
+    print("o calabreso ta ruim demais")
+    totalTentativas = 20
+elif (nivel == 2):
+    print("o calabreso tá mei bom em")
+    totalTentativas = 10
+elif(nivel == 3):
+    print("o calabreso ta bão em")
+    totalTentativas = 5
+
+for rodada in range (1, totalTentativas +1):
+    print("tentativa {} de {}".format(rodada,totalTentativas) )
+    chute_str = imput("Digite um número entre 1 e 100: ")
+    chute = int(chute_str)
+
+    if(chute < 1 or > 100):
+       print("Número invalido")
+       continue
+
+    acertou = chute == numeroSecreto
+    maior = chute > numeroSecreto
+    menor = chute < numeroSecreto
+
+    if(acertou):
+        print(f"Você acertou e fez {pontos}! ")
+        break
+    else:
+        if(maior):
+            print("Você errou! Seu chute foi maior que o numero secreto")
+        elif(menor):
+            print("Você errou! Seu chute foi menor que o numero secreto")
+
+            pontosPerdidos = abd(numeroSecreto - chute)
+            pontos = pontos - pontosPerdidos
+
+print("Fim de jogo ! O número era ",numeroSecreto)
+
+
+
+
